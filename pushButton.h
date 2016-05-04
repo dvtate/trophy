9cc038b8f0aa6eb54f0c75e40aabbdda58b141be
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-03 21:34:25 -0400

    Create pushButton.h

---
+++ pushButton.h
@@ -0,0 +1,16 @@
+#ifndef PUSH_BUTTON_H
+#define PUSH_BUTTON_H
+
+#include "Arduino.h"
+
+class PushButton {
+public:
+  uint8_t pin;
+  PushButton(uint8_t _pin):
+    pin(_pin) {}
+
+  
+  
+};
+
+#endif
