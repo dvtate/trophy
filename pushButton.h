f943e7951ba173dae0b1955b73723a3701b38ef8
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-05 14:27:25 -0400

    Update pushButton.h

--- pushButton.h
+++ pushButton.h
@@ -1,16 +1,26 @@
+
 #ifndef PUSH_BUTTON_H
 #define PUSH_BUTTON_H
 
 #include "Arduino.h"
 
+
+
 class PushButton {
 public:
-  uint8_t pin;
-  PushButton(uint8_t _pin):
-    pin(_pin) {}
+  uint8_t pin, bool value;
 
+  PushButton(uint8_t _pin):
+    pin(_pin), value(0) 
+  { pinMode(pin, INPUT); }
+
+
+  bool get()
+  { return (value = digitalRead(pin)); }
   
-  
+
 };
 
 #endif
+
+
