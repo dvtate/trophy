e9a41895b9bdd2ddc56e36e0ff0d509fec628d1a
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-06 19:21:13 -0400

    this shouldn't be an object...

--- pushButton.h
+++ pushButton.h
@@ -1,14 +1,14 @@
-
 #ifndef PUSH_BUTTON_H
 #define PUSH_BUTTON_H
 
 #include "Arduino.h"
 
-
+// this shouldn't be an object
 
 class PushButton {
 public:
-  uint8_t pin, bool value;
+  uint8_t pin;
+  bool value;
 
   PushButton(uint8_t _pin):
     pin(_pin), value(0) 
@@ -18,14 +18,17 @@ public:
   bool get()
   { return (value = digitalRead(pin)); }
   
-  void toggle(bool& out);
-
-  void toggle(uint8_t pin);
-
-  void toggle(void (*function)());
+  bool toggle(){
+    static bool previous = false;
+    
+    if (!digitalRead(pin) && previous)
+      return previous = false;
+    else if (digitalRead(pin) && !previous)
+      return previous = true;
+    
+    return false;
+  }
 
 };
 
 #endif
-
-
