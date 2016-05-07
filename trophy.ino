9cbcc771115f4fd50e5914bb27c8590b956dcd8a
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-07 15:32:24 -0400

    simplify...
    - more comments
    - patterns moved to "patterns.h"

--- trophy.ino
+++ trophy.ino
@@ -31,71 +31,8 @@ PushButton buzzButton(39);
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-
-
-// prototype functions to preavent errors
-extern bool checkInput();
-extern void nextPattern();
-extern void soundCheck();
-extern void resetLEDs();
-
-// a namespace filled with functions which are called to make the trophy light up and stuff
-namespace lightPatterns {
-  // the pattern that gets replaced by other patterns
-  void initial(){
-
-    // pattern code goes here...
-
-    rgb0.colorCycle(4);
-    flasher.seeSaw(16);
-    delay(5);
-    rgb0.colorCycle(4);
-    flasher.seeSaw(16);
-
-
-
-    // must be called periodically 
-    soundCheck();
-    //resetLEDs();
-
-    // don't call checkInput() here...
-      // (freezes leds in their current state)
-  }
-
-  void pattern1(){
-
-    // pattern code goes here...
-    rgb0.setColor(Color(255, 0, 0));
-
-    // should get called periodically
-    if (checkInput()) return;
-    soundCheck();
-  }
-  void pattern2(){
-    
-    // pattern code goes here...
-    rgb0.setColor(Color(0, 255, 0));
-
-    
-    // should get called periodically
-    if (checkInput()) return;
-    soundCheck();
-  }
-  void pattern3(){
-    
-    // pattern code goes here...
-    rgb0.setColor(Color(0, 0, 255));
-
-    
-    // should get called periodically
-    if (checkInput()) return;
-    soundCheck();
-  }
-}
-
-// sofar only 3 light patterns
-#define NUMBER_OF_PATTERNS 3
-
+// namespace lightpatterns & NUMBER_OF_PATTERNS
+#include "patterns.h"
 
 // the jump table of the different light patterns
 void (*patterns[NUMBER_OF_PATTERNS])(void) = {
@@ -147,7 +84,8 @@ void soundCheck(){
 }
 
 inline void resetLEDs(){
-
+  rgb0.setColor(Color(0,0,0));
+  flasher.set(0,0);
 }
 
 void setup(){
@@ -169,10 +107,8 @@ void loop(){
 
   //so simple :D
   currentPattern();
-  checkInput();
+  checkInput(); // might not be needed
   delay(5);
-
-  // just to make me happy :)
 
   /*
   Serial.print("Dist =");
