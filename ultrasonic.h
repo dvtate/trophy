aeb9a17c08e148bc5bfe1b509285ff9fd24de7fc
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-06 22:31:32 -0400

    add software timeout...

--- ultrasonic.h
+++ ultrasonic.h
@@ -22,7 +22,7 @@ public:
   
     // detect the echo from the pulse
     pinMode(pin, INPUT);
-    duration = pulseIn(pin, HIGH);
+    duration = pulseIn(pin, HIGH, 59200); // timeout of 400cm
   }
 
   // returns 796 upon timeout
@@ -38,9 +38,17 @@ public:
   
 };
 
+// According to Parallax's datasheet for the PING))), there are
+// 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+// second).  This gives the distance travelled by the ping, outbound
+// and return, so we divide by 2 to get the distance of the obstacle.
+// See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
 long microsecondsToCentimeters(const long& microseconds) // 0 ~ 400cm
   {return microseconds / 29 / 2;}
 
+// The speed of sound is 340 m/s or 29 microseconds per centimeter.
+// The ping travels out and back, so to find the distance of the
+// object we take half of the distance travelled.
 long microsecondsToInches(const long& microseconds) // 0 ~ 157 inches
   {return microseconds / 74 / 2;}
 
