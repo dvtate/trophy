6080a19f8262f77d19247d680a25e2652dacf7dd
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-07 18:23:33 -0400

    nothing noteable...

--- patterns.h
+++ patterns.h
@@ -1,7 +1,8 @@
 #ifndef PATTERNS_H
 #define PATTERNS_H
 
-// prototype functions to preavent errors
+// prototype functions to prevent errors
+// these functions get defined later in the file
 extern bool checkInput();
 extern void nextPattern();
 extern void soundCheck();
@@ -9,11 +10,11 @@ extern void resetLEDs();
 
 // a namespace filled with functions which are called to make the trophy light up and stuff
 namespace lightPatterns {
+  
   // the pattern that gets replaced by other patterns
   void initial(){
 
     // pattern code goes here...
-
     rgb0.colorCycle(4);
     flasher.seeSaw(16);
     delay(5);
@@ -23,45 +24,65 @@ namespace lightPatterns {
 
 
     // must be called periodically 
-    soundCheck();
-    //resetLEDs();
-
-    // don't call checkInput() here...
-      // (freezes leds in their current state)
+    soundCheck(); // should be called before checkInput();
+    if (checkInput()) return;
+    
   }
 
   void pattern1(){
 
+    // reset from previous pattern... (run once)
+    static bool initialized = false;
+    if (!initialized) {
+      resetLEDs();
+    }
+
     // pattern code goes here...
-    rgb0.setColor(Color(255, 0, 0));
+    rgb0.setColor(Color(255, 0, 0)); // for now this is it...
 
     // should get called periodically
     if (checkInput()) return;
     soundCheck();
   }
+  
   void pattern2(){
     
+    // reset from previous pattern... (run once)
+    static bool initialized = false;
+    if (!initialized) {
+      resetLEDs();
+    }
+    
     // pattern code goes here...
-    rgb0.setColor(Color(0, 255, 0));
+    rgb0.setColor(Color(0, 255, 0)); //for now this is it...
 
     
     // should get called periodically
-    if (checkInput()) return;
     soundCheck();
+    if (checkInput()) return;
+    
   }
+  
   void pattern3(){
     
+    // reset from previous pattern... (run once)
+    static bool initialized = false;
+    if (!initialized) {
+      resetLEDs();
+    }
+    
     // pattern code goes here...
-    rgb0.setColor(Color(0, 0, 255));
+    rgb0.setColor(Color(0, 0, 255)); //for now this is it...
 
     
     // should get called periodically
-    if (checkInput()) return;
     soundCheck();
+    if (checkInput()) return;
   }
+  
 }
 
-// sofar only 3 light patterns
+// so far only 3 light patterns
 #define NUMBER_OF_PATTERNS 3
 
 
