b64951ee41d464ca6ee99df3265844daef769926
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-13 18:23:19 -0400

    major changes coming soon...
    Essentially a restructuring of the way this file gets used.

--- patterns.h
+++ patterns.h
@@ -7,19 +7,30 @@ extern bool checkInput();
 extern void nextPattern();
 extern void soundCheck();
 extern void resetLEDs();
+extern void refreshLEDs();
+
 
 // a namespace filled with functions which are called to make the trophy light up and stuff
 namespace lightPatterns {
   
+  bool initialized[3] {false, false, false};
+
+  
   // the pattern that gets replaced by other patterns
   void initial(){
 
-    // pattern code goes here...
     rgb0.colorCycle(4);
-    flasher.seeSaw(16);
-    delay(4);
-    rgb0.colorCycle(4);
-    flasher.seeSaw(16);
+    rgb1.colorCycle(4);
+
+    static uint8_t cycles = 0;
+    
+    for (unsigned char i = 0; i < 4; i++)
+      if (!cycles++) {
+        redBlues[0][0].swap();
+        redBlues[0][1].swap();
+        redBlues[1][0].swap();
+        redBlues[1][1].swap();
+      }
 
 
     // must be called periodically 
@@ -28,31 +39,118 @@ namespace lightPatterns {
     
   }
 
-  void pattern1(){
+  void pattern0(){
 
     // reset from previous pattern... (run once)
     static bool initialized = false;
-    if (!initialized) {
+    if (!initialized[0]) {
+      initialized = true;
+      
       resetLEDs();
+      
+      rgb0.color.g = 255;
+      rgb1.color.g = 0;
+      
     }
 
-    // pattern code goes here...
-    rgb0.setColor(Color(255, 0, 0)); // for now this is it...
+    rgb1.colorCycle();
 
+    static bool curHi = 0;
+    color::cycle2(rgb0.color.g, rgb1.color.g, curHi);
+
+    
+    static uint8_t cycles = 0, activeLED0 = 0, activeLED1 = 0;
+
+    
+    // cycle
+    for (unsigned char i = 0; i < 4; i++)
+      if (!cycles++) {
+        if (activeLED0 == 5)
+          activeLED0 = 0;
+        else
+          activeLED0++;
+
+        if (activeLED1 == 5)
+          activeLED1 = 0;
+        else
+          activeLED1++;
+        
+      }
+    // cycle the blues    
+    switch (activeLED0) {
+    case 0: 
+      redBlues[0][0].v1 = !redBlues[0][0].v1;
+      break;
+
+    case 1: 
+      redBlues[0][1].v1 = !redBlues[0][1].v1;
+      break;
+
+    case 2:
+      rgb1.color.b = rgb1.color.b ? 0 : 255;
+      break;
+      
+    case 3:
+      rgb0.color.b = rgb0.color.b ? 0 : 255;
+      break;
+      
+    case 4: 
+      redBlues[1][1].v1 = !redBlues[1][1].v1;
+      break;
+
+    case 5: 
+      redBlues[1][0].v1 = !redBlues[1][0].v1;
+      break;
+
+    }
+
+    // cycle the reds
+    switch (activeLED1) {
+    case 5: 
+      redBlues[0][0].v0 = !redBlues[0][0].v0;
+      break;
+
+    case 4: 
+      redBlues[0][1].v0 = !redBlues[0][1].v0;
+      break;
+
+    case 3:
+      rgb1.color.r = rgb1.color.r ? 0 : 255;
+      break;
+      
+    case 2:
+      rgb0.color.r = rgb0.color.r ? 0 : 255;
+      break;
+      
+    case 1: 
+      redBlues[1][1].v0 = !redBlues[1][1].v0;
+      break;
+
+    case 0: 
+      redBlues[1][0].v0 = !redBlues[1][0].v0;
+      break;
+
+    }
+
+    
+
+    refreshLEDs();
+
+    
     // should get called periodically
     if (checkInput()) return;
-    soundCheck();
+    //soundCheck();
   }
   
-  void pattern2(){
+  void pattern1(){
     
     // reset from previous pattern... (run once)
     static bool initialized = false;
     if (!initialized) {
       resetLEDs();
+      initialized = true;
     }
     
-    // pattern code goes here...
     rgb0.setColor(Color(0, 255, 0)); //for now this is it...
 
     
@@ -62,12 +160,13 @@ namespace lightPatterns {
     
   }
   
-  void pattern3(){
+  void pattern2(){
     
     // reset from previous pattern... (run once)
     static bool initialized = false;
     if (!initialized) {
       resetLEDs();
+      initialized = true;
     }
     
     // pattern code goes here...
