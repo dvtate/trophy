41c994e8dbbd7298caddad4b0e2aa98d2d73d1df
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-14 16:38:56 -0400

    not done yet
    Major changes are nearing completion.

--- trophy.ino
+++ trophy.ino
@@ -11,27 +11,27 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-//hardware
+//hardware:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-TriLED rgbs[2] {
+TriLED top[2] {
   TriLED(13, 12, 11, Color(255, 0, 0)),
   TriLED(10, 9, 8, Color(255, 0, 0))
 };
 
-TriLED& rgb0 = rgbs[0], rgb1 = rgbs[1];
+TriLED& top0 = top[0], top1 = top[1];
 
 
-BiLED redBlues[2][2]{
-  BiLED(A0, A1, 255, 0), BiLED(A4, A5, 255, 0),
-  BiLED(A2, A3, 255, 0), BiLED(A6, A7, 255, 0)
+DigitalTriLED base[2][2]{
+  DigitalTriLED(48, 49, 50), DigitalTriLED(46,47,51),
+  DigitalTriLED(44, 45, 52), DigitalTriLED(42, 43, 53)
 };
 
 
 
 // the ultrasonic sensor / user interface
-Ultrasonic sonar(52);
+Ultrasonic sonar(A0);
 
 
 // buzzer
@@ -48,33 +48,34 @@ bool audioEnabled = true;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-// namespace lightpatterns & NUMBER_OF_PATTERNS
-#include "patterns.h"
-
-// the jump table of the different light patterns
-void (*patterns[NUMBER_OF_PATTERNS])(void) = {
-  lightPatterns::pattern0,
-  lightPatterns::pattern1,
-  lightPatterns::pattern2
-};
-
-// the currently selected light pattern
-void (*currentPattern)(void) = lightPatterns::initial;
+#include "patterns.h" // the definitions for the patterns
+#include "pattern.h" // the common functions for patterns
 
 
-// choose a different pattern
-void nextPattern(){
 
-  static uint8_t patternNumber = 0;
 
-  if (patternNumber < (NUMBER_OF_PATTERNS - 1))
-    patternNumber++;
-  else
-    patternNumber = 0;
-
-  currentPattern = patterns[patternNumber];
-
+inline void resetLEDs(){
+  top[0].set(Color(0,0,0));
+  top[1].set(Color(0,0,0));
+  
+  base[0][0].set(0, 0, 0);
+  base[0][1].set(0, 0, 0);
+  base[1][0].set(0, 0, 0);
+  base[1][1].set(0, 0, 0);
+  
 }
+inline void refreshLEDs(){
+  top[0].refresh();
+  top[1].refresh();
+  
+  base[0][0].refresh();
+  base[0][1].refresh();
+  base[1][0].refresh();
+  base[1][1].refresh();
+}
+
+
+
 
 // returns true when it's time to switch patterns
 bool checkInput(){
@@ -84,7 +85,7 @@ bool checkInput(){
   // if user's hand is detected
   if (previous && sonar.getCm() < 20) {
 
-    nextPattern();
+    pickNextPattern();
 
     if (audioEnabled) // ring 150Hz Square tone for 10ms
       tone(BUZZPIN, 150, 10); 
@@ -100,50 +101,18 @@ void soundCheck(){
   digitalWrite(BUZZLEDPIN, audioEnabled);
 }
 
-inline void resetLEDs(){
-  
-  rgbs[0].set(Color(0,0,0));
-  rgbs[1].set(Color(0,0,0));
-  
-  redBlues[0][0].set(0, 0);
-  redBlues[0][1].set(0, 0);
-  redBlues[1][0].set(0, 0);
-  redBlues[1][1].set(0, 0);
-  
-}
-inline void refreshLEDs(){
-  rgbs[0].refresh();
-  rgbs[1].refresh();
-  
-  redBlues[0][0].refresh();
-  redBlues[0][1].refresh();
-  redBlues[1][0].refresh();
-  redBlues[1][1].refresh();
-}
 
 void setup(){
-  
+
+ 
   // disable pull-up resistor
   pinMode(BUZZLEDPIN, OUTPUT);
 
-  for (;1;) { // infinite crying
-    currentPattern(); // call initial pattern until user input is recieved
-    delay(4);
-
-  }
-
-  resetLEDs();
-  
 }
 
 void loop(){
 
-  //so simple :D
-  currentPattern();
 
- // called in currentPattern()  
-  soundCheck();
-//  checkInput();
 
   delay(5);
 
