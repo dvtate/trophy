861a1c2b8a08b7aa431be4414f0aea85ba356c9a
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-14 21:12:02 -0400

    cycle2 bugfix
    inlining some functions to save resources...

--- color.h
+++ color.h
@@ -11,7 +11,11 @@ public:
   Color(): r(0), g(0), b(0) {}
   Color(const uint8_t& _r, const uint8_t& _g, const uint8_t& _b):
     r(_r), g(_g), b(_b) 
-  {}
+  { }
+
+  Color(const uint8_t& val) : 
+    r(val), g(val), b(val) 
+  { }
 
   Color& invert(){
     r = 255 - r;
@@ -30,18 +34,16 @@ public:
 };
 
 
-
 namespace color {
 
-  
-  Color invert(Color c){
+  inline Color& invert(Color& c){
     c.r = 255 - c.r;
     c.g = 255 - c.g;
     c.b = 255 - c.b;
     return c;
   }
 
-  void cycle3(uint8_t& v0, uint8_t& v1, uint8_t& v2, uint8_t& curHi){
+  inline void cycle3(uint8_t& v0, uint8_t& v1, uint8_t& v2, uint8_t& curHi){
     if (curHi == 0) {
       v0--; v1++;
     } else if (curHi == 1) {
@@ -58,29 +60,32 @@ namespace color {
       curHi = 0;
   }
 
-  void cycle2(uint8_t& v0, uint8_t& v1, bool& curHi){
-    if (!curHi) {
+  inline void cycle2(uint8_t& v0, uint8_t& v1, bool& curHi){
+    if (v0 == 0)
+      curHi = 1;
+    else if (v1 == 0)
+      curHi = 0;
+    
+    if (curHi == 0) {
       v0--; v1++;
     } else {
-      v1--; v0--;
+      v1--; v0++;
     }
-    
-    if (v0 == 0 || !v1 == 0)
-      curHi = !curHi;
 
   }
   
-  void cycle2(uint8_t& v0, uint8_t& v1, bool& curHi, uint8_t incr){
+  inline void cycle2(uint8_t& v0, uint8_t& v1, bool& curHi, uint8_t incr){
     while (incr--) {
-      if (!curHi) {
+      if (v0 == 0)
+        curHi = 1;
+      else if (v1 == 0)
+        curHi = 0;
+      
+      if (curHi == 0) {
         v0--; v1++;
       } else {
-        v1--; v0--;
+        v1--; v0++;
       }
-      
-      if (v0 == 0 || !v1 == 0)
-        curHi = !curHi;
-        
     }
   }
 }
