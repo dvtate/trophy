57bd30d1158bd714fe61b0c181a586d73f2bb560
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-20 18:06:07 -0400

    add base colors ppc's

--- color.h
+++ color.h
@@ -90,4 +90,13 @@ namespace color {
   }
 }
 
+// base colors
+#define COLOR_RED Color(255, 0, 0)
+#define COLOR_YELLOW Color(255, 255, 0)
+#define COLOR_GREEN Color(0, 255, 0)
+#define COLOR_CYAN Color(0, 255, 255)
+#define COLOR_BLUE Color(0, 0, 255)
+#define COLOR_PURPLE Color(255, 0, 255)
+#define COLOR_WHITE Color(255, 255, 255)
+
 #endif
