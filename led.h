5a233ddb025d90f122565e6c0215eb5ca031d9bc
Dustin Van Tate Testa <toast27@gmail.com>
2016-06-07 08:59:22 -0400

    still incomplete

--- led.h
+++ led.h
@@ -288,24 +288,28 @@ public:
     pinMode(pg, OUTPUT);
     pinMode(pb, OUTPUT);
   }
+ 
   void setColor(const bool& red, const bool& green, const bool& blue){
     set(red, green, blue);
     refresh();
   }
+ 
   void setColor(const Color& clr){
     r = clr.r;
     g = clr.g;
     b = clr.b;
     refresh();
   }
-  
+ 
   void setColor()
     { refresh(); }
 
-  void set(const Color& clr)
-    { setColor(clr); }
   void set()
     { setColor(); }
+
+  void set(const Color& clr)
+    { setColor(clr); }
+
 
   void set(const bool& red, const bool& green, const bool& blue){
     r = red;
@@ -314,6 +318,7 @@ public:
     setColor(); // write/apply changes
   }
   
+  // apply a color
   void set(const bool& val){
     r = val;
     g = val;
@@ -321,11 +326,14 @@ public:
     refresh();
   }
 
+  // pushes values but doesn't refresh
   void push(const Color& clr){
     r = clr.r;
     g = clr.g;
     b = clr.g;
   }
+  
+  // burns in values
   void refresh(){
     digitalWrite(pr, r);
     digitalWrite(pg, g);
@@ -339,6 +347,7 @@ public:
     digitalWrite(pb, 0);
   }
   
+  // set all values to 0 (no refresh)
   void setNull(){
     r = LOW;
     g = LOW;
@@ -346,20 +355,23 @@ public:
   }
   
   void colorCycle(char order[4]){
+    // determine which pin is on HIGH
     char curHi = r ? 
         'r' : g ? 
           'g' : b? 
             'b': *order;
 
-    
+    // find fist occurance of current color (this is difficult to achieve)
     uint8_t i = 0;
     while (curHi != *(order + i) && i <= 4)
       i++;
     
-    i++;
+    i++; // we want the next color to change to
     
+    // prevents errors
     curHi = (i < 3) ? *(order + i) : *order;
 
+    // set correct color
     switch (curHi) {
     case 'r':
       r = HIGH;
@@ -385,20 +397,28 @@ public:
         
   }
 
+  // follow a scheme string to cycle between color values
   void colorCycle(char* order, uint8_t len){
+      
+    // determine curhi based on which color is currently HIGH
     char curHi = r?
         'r' : g? 
           'g' : b? 
             'b': *order;
 
     
+    
+    // find position in string where we are currrently (very inaccurate)
     uint8_t i = 0;
     while (curHi != *(order + i) && i < len)
       i++;
-    i++;
     
+    i++; // we want the color after where we are..
+
+    // just to prevent problems
     curHi = (i < len) ? *(order + i) : *order;
 
+    // set in the correct value
     switch (curHi) {
     case 'r':
       r = HIGH;
@@ -424,10 +444,13 @@ public:
     
   }
 
+  // cycle rgb
   void colorCycle(){
-    
+      
+    // determine nextHi based on which color is HIGH
     char nextHi = g ? 'b' : ( b? 'r': 'g');
 
+    // set the LED correct color
     switch (nextHi) {
     case 'r':
       r = HIGH;
@@ -447,9 +470,11 @@ public:
       b = HIGH;
       break;
     }
+    
     refresh();
   }
 
+  // invert current color
   void invert(){
     r = !r;
     g = !g;
@@ -473,9 +498,12 @@ public:
 
 
 
-// will no longer get used...
+// these will no longer get used...
+// but they're still useful...
 
-class BiLED { // digital
+
+// 2 digital LEDs
+class BiLED {
 public:
   unsigned int p0 : 7, p1 : 7;
   bool v0 : 1, v1 : 1; // could be replaced by uint8_t for PWM output
@@ -486,6 +514,7 @@ public:
     pinMode(p0, OUTPUT);
     pinMode(p1, OUTPUT);
   }
+  
   BiLED(const uint8_t& pin0, const uint8_t& pin1, const bool& val0, const bool& val1):
     p0(pin0), p1(pin1), v0(val0), v1(val1)
   {
@@ -493,15 +522,20 @@ public:
     pinMode(p1, OUTPUT);
   }
 
+  // burn in values
   void refresh(){
     digitalWrite(p0,v0);
     digitalWrite(p1,v1);
   }
+  
+  // push and apply values
   void set(const uint8_t& val0, const uint8_t& val1){
     v0 = val0;
     v1 = val1;
     refresh();
   }
+  
+  // push and apply the same value for v0 & v1
   void set(const uint8_t& val){
     v0 = val;
     v1 = val;
@@ -514,15 +548,31 @@ public:
     digitalWrite(p1, 0);
   }
   
+  // replace v0 and v1
   void swap(){
     bool temp = v0;
     v0 = v1;
     v1 = temp;
     refresh();
   }
+  
+  
+  // this will never get used...
+  void swapPins(uint8_t pin0, uint8_t pin1){
+    // note: this doesn't set(0) before swapping the pins.
+    
+    p0 = pin0;
+    p1 = pin1;
+    
+    // set them as output
+    pinMode(p0, OUTPUT);
+    pinMode(p1, OUTPUT);
+    // burn in current color
+    setColor();
+  }
 };
 
-// 2 leds with PWM capabilities
+// 2 LEDs with PWM capabilities
 class BiLED_pwm {
 public:
   uint8_t p0, p1; // making this a bitfield wouldn't improve anything
@@ -606,6 +656,19 @@ public:
     refresh();
   }
   
+  // this will never get used...
+  void swapPins(uint8_t pin0, uint8_t pin1){
+    // note: this doesn't set(0) before swapping the pins.
+    
+    p0 = pin0;
+    p1 = pin1;
+    
+    // set them as output
+    pinMode(p0, OUTPUT);
+    pinMode(p1, OUTPUT);
+    // burn in current color
+    setColor();
+  }
 };
 
 
