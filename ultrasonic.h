4c83e52b8cfa74d3448b69bd9433fc3e9627ec50
Dustin Van Tate Testa <toast27@gmail.com>
2016-06-07 16:49:25 -0400

    minor improvements to unused functions
    and I removed those huge comments

--- ultrasonic.h
+++ ultrasonic.h
@@ -82,18 +82,10 @@ public:
   
 };
 
-// According to Parallax's datasheet for the PING))), there are
-// 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-// second).  This gives the distance travelled by the ping, outbound
-// and return, so we divide by 2 to get the distance of the obstacle.
-// See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-long microsecondsToCentimeters(const long& microseconds) // 0 ~ 400cm
+inline long microsecondsToCentimeters(const long& microseconds) // 0 ~ 400cm
   { return microseconds / 29 / 2; }
 
-// The speed of sound is 340 m/s or 29 microseconds per centimeter.
-// The ping travels out and back, so to find the distance of the
-// object we take half of the distance travelled.
-long microsecondsToInches(const long& microseconds) // 0 ~ 157 inches
+inline long microsecondsToInches(const long& microseconds) // 0 ~ 157 inches
   { return microseconds / 74 / 2; }
 
 #endif
